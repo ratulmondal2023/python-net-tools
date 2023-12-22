31ed5770cfe73889564448d080373fdb206edda4
Ratul Mondal <150381310+ratulmondal2023@users.noreply.github.com>
2023-12-23 04:14:38 +0600

    Update tcp_client.py

--- Python-Network-Basics/tcp_client.py
+++ Python-Network-Basics/tcp_client.py
@@ -3,16 +3,12 @@ import socket
 target_host = "www.devanon.netlify.app"
 target_port = 80
 
-# Create a socket object
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-# Connect the client
 client.connect((target_host,target_port))
 
-# Send some data
 client.send(b"GET /HTTP/1.1\r\nHost: devanon.netlify.app\r\n\r\n")
 
-# Receive some data
 response = client.recv(4096)
 
 print(response.decode())
